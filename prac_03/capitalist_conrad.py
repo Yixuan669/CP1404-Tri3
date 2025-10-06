5faab073e21bf8b5625d94f49e590dc7b02acfe5
Frank <yixuan.zhang4@my.jcu.edu.au>
2025-10-06 20:01:32 +0800

    use random function and write the output into a new created file that created by this program.

---
+++ prac_03/capitalist_conrad.py
@@ -0,0 +1,31 @@
+import random
+dir(random)
+
+MAX_INCREASE = 0.175
+MAX_DECREASE = 0.05
+MIN_PRICE = 0.01
+MAX_PRICE = 1000.0
+INITIAL_PRICE = 10.0
+FILENAME = "capitalist_conrad.txt"
+
+def main():
+    price = INITIAL_PRICE
+    number_of_days = 0
+
+    out_file = open(FILENAME, 'w')
+
+    print(f"Starting price: ${price:,.2f}", file=out_file)
+
+    while MIN_PRICE <= price <= MAX_PRICE:
+        number_of_days += 1
+        if random.randint(1, 2) == 1:
+            price_change = random.uniform(0, MAX_INCREASE)
+        else:
+            price_change = -random.uniform(0, MAX_DECREASE)
+
+        price *= (1 + price_change)
+        print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+    out_file.close()
+
+main()
