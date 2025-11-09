36af2f83df030f5b354c442ae18bf951e43fab32
Frank <yixuan.zhang4@my.jcu.edu.au>
2025-11-09 18:27:49 +0800

    Move class to project_management and add import function allow project can grab the project class from project_management.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,4 +1,5 @@
 from datetime import datetime, date
+from project_management import Project
 
 FILENAME = "projects.txt"
 
@@ -9,26 +10,6 @@ MENU = """- (L)oad projects
 - (A)dd new project  
 - (U)pdate project
 - (Q)uit"""
-
-class Project:
-    """Create a class for this project"""
-    def __init__(self, name: str, start_date: date, priority: int, cost_estimate: float, completion_percentage: int):
-        self.name = name
-        self.start_date = start_date
-        self.priority = priority
-        self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
-
-    def is_complete(self) -> bool:
-        """Determine if this project is complete"""
-        return self.completion_percentage == 100
-
-    def __str__(self) -> str:
-        """Return a string representation of this project"""
-        date_str = self.start_date.strftime("%d/%m/%Y")
-        return (f"{self.name}, start: {date_str}, priority {self.priority}, "
-                f"estimate: ${self.cost_estimate:.2f}, "
-                f"completion: {self.completion_percentage}%")
 
 def main():
     """Create and run the main function"""
