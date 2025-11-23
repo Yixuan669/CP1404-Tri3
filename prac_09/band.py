1eaf9cfa6db602161832091a3663210ce0a5c00e
Frank <yixuan.zhang4@my.jcu.edu.au>
2025-11-23 16:52:00 +0800

    Return a string representation of the Band and its musicians.

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,3 +7,7 @@ class Band:
         """Initialise a Band with a name and an empty list of musicians."""
         self.name = name
         self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of the Band and its musicians."""
+        return f"{self.name} ({', '.join(str(m) for m in self.musicians)})"
