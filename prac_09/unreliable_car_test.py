674b616fd1d4b304dcf53b57ca751c6e48a66e03
Frank <yixuan.zhang4@my.jcu.edu.au>
2025-11-23 16:00:08 +0800

    Try drive this car 20 km for 10 times.

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,3 +1,7 @@
 from unreliable_car import UnreliableCar
 
 my_bad_car = UnreliableCar("Unreliable", 100, 50)
+
+for i in range(1, 11):
+    drive_distance = my_bad_car.drive(20)
+    print(f"Attempt {i}: Tried to drive 20km, actually drove {drive_distance}km")
