773ca273f3a612ce1b97d037d75548333af06d5c
Frank <yixuan.zhang4@my.jcu.edu.au>
2025-11-23 16:14:54 +0800

    Create the output by __str__ function.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -11,3 +11,7 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        """String representation of the SilverServiceTaxi"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
