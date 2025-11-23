ec4bdba5158adc227a4f140793048018f71a1397
Frank <yixuan.zhang4@my.jcu.edu.au>
2025-11-23 16:18:37 +0800

    Start first fare and drive for 18 km.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,3 +4,7 @@ from silver_service_taxi import SilverServiceTaxi
 def main():
     """Test SilverServiceTaxi fares."""
     taxi = SilverServiceTaxi("Test Taxi", 100, 2)
+
+    taxi.start_fare()
+    taxi.drive(18)
+    fare = taxi.get_fare()
